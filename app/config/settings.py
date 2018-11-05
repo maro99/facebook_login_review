311fed71c60b1cfefa91562d9966e77647eb65b6
nadcdc <nadcdc4@gmail.com>
2018-11-05 16:30:03 +0900

    Facebook api, secret key , id  .secrets으로 빼줌.

--- app/config/settings.py
+++ app/config/settings.py
@@ -11,16 +11,25 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import json
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # app
+ROOT_DIR = os.path.dirname(BASE_DIR)  # project
+SECRET_DIR = os.path.join(ROOT_DIR,'.secrets')
+secrets = json.load(open(os.path.join(SECRET_DIR,'base.json')))
+
+
+FACEBOOK_APP_ID = secrets['FACEBOOK_APP_ID']
+FACEBOOK_APP_SECRET_CODE = secrets["FACEBOOK_APP_SECRET_CODE"]
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't8b^to!6^^kj&%ley%5kigt=bb_&(*_5xo&gbsyp29$#!dy)44'
+SECRET_KEY = secrets["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
